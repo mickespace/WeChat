74d4232a1db92d4bc9f25be625a19ac635d29c94
mickespace <micke_space@163.com>
2017-06-16 18:19:43 +0800

    修改消息集合

--- Mango/pages/message/message.js
+++ Mango/pages/message/message.js
@@ -1,10 +1,25 @@
+var initData = [{ Name: '大爷', Content: 'fuck' }, { Name: '大爷', Content: 'fuck' }, { Name: '大爷', Content: 'fuck' }, { Name: '大爷', Content: 'fuck' }]
+
 Page({
   data: {
-    MessageList: [{ Name: '大爷', Content: 'fuck' }, { Name: '大爷', Content: 'fuck' }, { Name: '大爷', Content: 'fuck' }, { Name: '大爷', Content: 'fuck'}]
+    MessageList: initData
   },
-  onLoad: function () {
+  onShow: function () {
    //加载数据
-   
+   wx.showLoading({
+     title: '加载数据',
+   })
+   this.setData({
+     MessageList: [{ Name: '大爷1', Content: 'fuck1' }, { Name: '大爷', Content: 'fuck1' }, { Name: '大爷', Content: 'fuck1' }, { Name: '大爷1', Content: 'fuck1' }]
+   })
+   wx.showToast({
+     title: 'shabi',
+   })
+  },
+  Refesh:function(){
+    wx.showToast({
+      title: 'shabi',
+    })
   },
   onShareAppMessage:function(){
 
